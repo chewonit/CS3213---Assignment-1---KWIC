5f5108f1aafd14494cdb0bfb420cc5d366b15ed6
Darry Chew <darry@nus.edu.sg>
2014-08-23 20:52:14 +0800

    JUnit Test Cases for Line Class

--- src/kwic_test/LineTest.java
+++ src/kwic_test/LineTest.java
@@ -49,4 +49,117 @@ public class LineTest {
 		}
 	}
 
+	@Test
+	public void testSize() {
+		Vector<String> words = new Vector<String>();
+		words.add("Hello");
+
+		Line line = new Line(1, words);
+		assertEquals(1, line.size());
+
+		words.add("World");
+		line = new Line(1, words);
+		assertEquals(2, line.size());
+
+		for (int i = 0; i < 1000; i++)
+			words.add("dummy");
+		line = new Line(1, words);
+		assertEquals(1002, line.size());
+	}
+
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void testGetWordIndexOutOfBounds() {
+		Vector<String> words = new Vector<String>();
+		words.add("Hello");
+
+		Line line = new Line(1, words);
+		line.getWord(1);
+	}
+
+	@Test
+	public void testGetWord() {
+		Vector<String> words = new Vector<String>();
+		words.add("Hello");
+
+		Line line = new Line(1, words);
+		assertEquals("Hello", line.getWord(0));
+
+		words.add("World");
+		line = new Line(1, words);
+		assertEquals("World", line.getWord(1));
+
+		for (int i = 0; i < 1000; i++)
+			words.add("dummy");
+		line = new Line(1, words);
+		assertEquals("dummy", line.getWord(954));
+	}
+
+	@Test
+	public void testFirstWord() {
+		Vector<String> words = new Vector<String>();
+		words.add("Hello");
+
+		Line line = new Line(1, words);
+		assertEquals("Hello", line.getFirstWord());
+
+		words.add("World");
+		line = new Line(1, words);
+		assertEquals("Hello", line.getFirstWord());
+
+		for (int i = 0; i < 1000; i++)
+			words.add("dummy");
+		line = new Line(1, words);
+		assertEquals("Hello", line.getFirstWord());
+	}
+
+	@Test
+	public void testSetFiltered() {
+		Vector<String> words = new Vector<String>();
+		words.add("Hello");
+		words.add("World");
+
+		Line line = new Line(1, words);
+
+		Field field;
+		try {
+			field = Line.class.getDeclaredField("isFiltered");
+			field.setAccessible(true);
+
+			boolean value = (boolean) field.get(line);
+			assertFalse(value);
+
+			line.setFiltered(true);
+			value = (boolean) field.get(line);
+			assertTrue(value);
+
+		} catch (Exception e) {
+			fail("Could not access private variable.");
+		}
+	}
+
+	@Test
+	public void testIsFiltered() {
+		Vector<String> words = new Vector<String>();
+		words.add("Hello");
+		words.add("World");
+
+		Line line = new Line(1, words);
+		assertFalse(line.isFiltered());
+
+		line.setFiltered(true);
+		assertTrue(line.isFiltered());
+	}
+	
+	@Test
+	public void testToString() {
+		Vector<String> words = new Vector<String>();
+		words.add("Hello");
+
+		Line line = new Line(1, words);
+		assertEquals("Hello", line.toString());
+		
+		words.add("World");
+		line = new Line(1, words);
+		assertEquals("Hello World", line.toString());
+	}
 }
