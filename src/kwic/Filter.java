3ae8ec9a5b121d59e116f919f6c772a63c40e549
Tee Ming <teemingchew@gmail.com>
2014-08-24 16:33:49 +0800

    Updated Filter class to Singleton class

--- src/kwic/Filter.java
+++ src/kwic/Filter.java
@@ -11,25 +11,52 @@ import java.util.*;
 
 public class Filter {
 	
+	/** Singleton Instance */
+	private static Filter instance = null;
 	private HashMap<String, String> ignoredWords;
 	
-	public Filter(){
+	private Filter(){
 		ignoredWords = new HashMap<String, String>();
 	}
 	
+	/**
+	 * Add a word to ignored list
+	 * @param inputWord
+	 */
 	public void addFilter(String inputWord){
 		ignoredWords.put(inputWord, inputWord);
 	}
 	
+	/**
+	 * Remove a word from ignored list
+	 * @param inputWord
+	 */
 	public void removeFilter(String inputWord){
 		ignoredWords.remove(inputWord);
 	}
 	
+	/**
+	 * Check if a word is in the ignored list
+	 * @param inputWord
+	 * @return result of whether the word is in ignored list
+	 */
 	public boolean isWordFiltered(String inputWord){
 		if(inputWord != null){
 			return ignoredWords.containsKey(inputWord);
 		} else {
 			return false;
 		}
+	}
+	
+	/**
+	 * Get the singleton instance of the Filter Class.
+	 * 
+	 * @return instance of the Filter Class.
+	 */
+	public static Filter getInstance() {
+		if (instance == null) {
+			instance = new Filter();
+		}
+		return instance;
 	}
 }
