70f0b6d3c7f2a674e32964482801e0f441b8cf78
Darry Chew <darry@nus.edu.sg>
2014-08-26 22:05:56 +0800

    GUI updated to use List with DefaultListModel for displaying the output.

--- src/kwic_gui/KWIC_GUI.java
+++ src/kwic_gui/KWIC_GUI.java
@@ -2,15 +2,21 @@ package kwic_gui;
 
 import java.awt.EventQueue;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.DefaultListModel;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.ListSelectionModel;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 
@@ -20,10 +26,9 @@ public class KWIC_GUI extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField textField;
-	private JTextField textField_1;
 	private JLabel lblNewLabel;
 	
-	private KWIC kwic;
+	private static KWIC kwic;
 
 	/**
 	 * Launch the application.
@@ -41,7 +46,27 @@ public class KWIC_GUI extends JFrame {
 			}
 		});
 	}
+	
+	private void clearTextField(JTextField textField) {
+		textField.setText("");
+	}
+	
+	private void toggleButtonEnabled(JButton btn) {
+		if( btn.isEnabled() ) {
+			btn.setEnabled(false);
+		} else {
+			btn.setEnabled(true);
+		}
+	}
+	
+	private void updateDisplayList(DefaultListModel listModel) {
 
+		listModel.clear();
+		listModel.addElement("asdsad");
+		listModel.addElement("helloworld");
+		
+	}
+	
 	/**
 	 * Create the frame.
 	 */
@@ -59,26 +84,54 @@ public class KWIC_GUI extends JFrame {
 		textField = new JTextField();
 		textField.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
+		lblNewLabel = new JLabel("Line:");
 		
-		lblNewLabel = new JLabel("New Line:");
+		JScrollPane scrollPane = new JScrollPane();
+		final JList<String> list = new JList<String>();
+		final DefaultListModel<String> listModel = new DefaultListModel<String>();
 		
-		JButton btnSubmit = new JButton("Submit");
+		scrollPane.setViewportView(list);
+		list.setModel(listModel);
+		
+		final JButton btnAdd = new JButton("Add");
+		JButton btnDeleteSelected = new JButton("Delete selected lines");
+		btnDeleteSelected.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println(list.getSelectedValue());
+				System.out.println(list.getSelectedIndex());
+				
+				updateDisplayList(listModel);
+			}
+		});
+		
+		btnAdd.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String input = textField.getText();
+				clearTextField( textField );
+				toggleButtonEnabled( btnAdd );
+				
+				kwic.process( input );
+				
+				toggleButtonEnabled( btnAdd );
+				updateDisplayList( listModel );
+			}
+		});
+		
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.TRAILING)
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
 					.addContainerGap()
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addComponent(textField, GroupLayout.DEFAULT_SIZE, 738, Short.MAX_VALUE)
 						.addComponent(lblKwic, GroupLayout.DEFAULT_SIZE, 738, Short.MAX_VALUE)
-						.addGroup(gl_contentPane.createSequentialGroup()
+						.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 738, Short.MAX_VALUE)
+						.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
 							.addComponent(lblNewLabel)
 							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(textField_1, GroupLayout.DEFAULT_SIZE, 569, Short.MAX_VALUE)
+							.addComponent(textField, GroupLayout.DEFAULT_SIZE, 569, Short.MAX_VALUE)
 							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(btnSubmit)))
+							.addComponent(btnAdd))
+						.addComponent(btnDeleteSelected, Alignment.TRAILING))
 					.addContainerGap())
 		);
 		gl_contentPane.setVerticalGroup(
@@ -86,15 +139,19 @@ public class KWIC_GUI extends JFrame {
 				.addGroup(gl_contentPane.createSequentialGroup()
 					.addContainerGap()
 					.addComponent(lblKwic)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(textField, GroupLayout.PREFERRED_SIZE, 318, GroupLayout.PREFERRED_SIZE)
-					.addGap(13)
+					.addGap(30)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
 						.addComponent(lblNewLabel)
-						.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnSubmit))
-					.addContainerGap(129, Short.MAX_VALUE))
+						.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnAdd))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 322, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(btnDeleteSelected)
+					.addContainerGap(61, Short.MAX_VALUE))
 		);
+		
+		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		contentPane.setLayout(gl_contentPane);
 	}
 }
