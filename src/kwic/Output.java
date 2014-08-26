b852f347b267d5e795ca399dfd60d0c4e6233b4c
Tee Ming <teemingchew@gmail.com>
2014-08-26 16:48:14 +0800

    Implemented Output class

--- src/kwic/Output.java
+++ src/kwic/Output.java
@@ -1,5 +1,7 @@
 package kwic;
 
+import java.util.Vector;
+
 /**
  * 
  * 
@@ -8,5 +10,20 @@ package kwic;
  */
 
 public class Output {
-
+	private Storage storage;
+	public Output(){
+		storage = Storage.getInstance();
+	}
+	
+	public Vector<String> output(){
+		Vector<String> formattedOutput = new Vector<String>();
+		for(int i = 0; i < storage.size(); i++){
+			Line retrievedLine = storage.getLine(i);
+			if(retrievedLine.size() > 0){
+				String retrievedString = retrievedLine.toString();
+				formattedOutput.add(retrievedString);
+			}
+		}
+		return formattedOutput;
+	}
 }
